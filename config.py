d1dfa3803d24cc1c3fb917c83d9a172238050b9a
Patrik Dendis <patrik@dendis.sk>
2020-01-06 16:38:39 +0100

    Updated config

--- config.py
+++ config.py
@@ -6,11 +6,9 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
-APNS_CERTIFICATE = 'prodnopass.pem'
+APNS_CERTIFICATE = 'test.pem'
 APNS_SANDBOX = False
-# APNS_CERTIFICATE = 'devnopass.pem'
-# APNS_SANDBOX = True
-GCM_API_KEY = 'AIzaSyBv7nan24gvGBZN_aXz01J-x8IpWBWsK3c'
+GCM_API_KEY = '<some_key>'
 JOBS = [
         {
             'id': 'first',
